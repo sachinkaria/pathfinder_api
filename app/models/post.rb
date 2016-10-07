0e1867ec09fd323d18c69194b1ddb652f34d3472
Sachin Karia <sachin.karia01@gmail.com>
2016-10-07 13:46:43 +0100

    added username column to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   def as_json(options={})
-    super(only: [:description])
+    super(only: [:description, :created_at, :user_id, :username])
   end
 end
